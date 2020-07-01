75529522554c607d174c70fb91ad99c94f928a23
nitrohub <nitesh.h@somaiya.edu>
2020-07-01 14:41:18 +0530

    First App

---
+++ helloworld/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <h1>Hello Nitesh</h1>
+          -First react App
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
